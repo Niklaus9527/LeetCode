26a4f41e963e20414c7100a1a384b13e563e0bdc
klaus <87357098@qq.com>
2017-02-10 01:50:44 +0800

    add 001 readme

--- src/main/java/OJ001_TwoSum/TwoSum.java
+++ src/main/java/OJ001_TwoSum/TwoSum.java
@@ -12,10 +12,21 @@ public class TwoSum {
         for(int i = 0; i < nums.length; i++){
             int temp = target - nums[i];
             if(map.containsKey(temp)){
-                return new int[]{map.get(temp), i};
+                return new int[]{ map.get(temp), i};
             }
-            map.put(nums[i], i);
+            map.put( nums[i], i );
         }
         throw new IllegalArgumentException("no two sum solution!");
     }
+    public int[] twoSumFirstTime(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("no two sum solution!");
+    }
+
 }
